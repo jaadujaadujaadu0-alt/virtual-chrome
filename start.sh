be9607ea73614b507a199216b1e681ada738a855
jaadujaadujaadu0-alt <jaadujaadujaadu0@gmail.com>
2026-04-30 15:03:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Start virtual display
 Xvfb :99 -screen 0 1280x800x16 &
-
 export DISPLAY=:99
 
-# Start window manager
 fluxbox &
 
-# Start Chromium
-chromium-browser --no-sandbox --disable-gpu --disable-dev-shm-usage &
+# ✅ Use correct binary
+chromium --no-sandbox --disable-dev-shm-usage --disable-gpu &
 
-# Start VNC server
 x11vnc -display :99 -nopw -forever -shared &
 
-# Start noVNC (Web access)
 websockify --web=/usr/share/novnc 0.0.0.0:$PORT localhost:5900
